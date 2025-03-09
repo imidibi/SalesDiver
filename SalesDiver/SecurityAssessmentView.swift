95805871b101e24dacaa993521b5cd22aa7cb651
Ian Miller <ianmiller@mac.com>
2025-03-09 18:41:08 -0400

    Security icon colors working

--- SalesDiver/SecurityAssessmentView.swift
+++ SalesDiver/SecurityAssessmentView.swift
@@ -11,6 +11,30 @@ struct SecurityAssessmentView: View {
         "Passwords": false, "SIEM & SOC": false, "Firewall": false, "DNS Protection": false,
         "Multi-Factor Authentication": false, "Computer Updates": false, "Encryption": false, "Cyber Insurance": false
     ]
+    @State private var selectedStatus: [String: Status] = [:]
+    @State private var showStatusSelection: Bool = false
+    @State private var statusToUpdate: String? = nil
+    @State private var showErrorMessage: Bool = false
+
+    enum Status: CaseIterable {
+        case protected, inProgress, atRisk
+        
+        var color: Color {
+            switch self {
+            case .protected: return .green
+            case .inProgress: return .yellow
+            case .atRisk: return .red
+            }
+        }
+        
+        var description: String {
+            switch self {
+            case .protected: return "Protected"
+            case .inProgress: return "In Progress"
+            case .atRisk: return "At Risk"
+            }
+        }
+    }
 
     let securityOptions: [(name: String, icon: String)] = [
         ("Security Assessment", "shield.fill"), ("Security Awareness", "person.2.fill"), ("Dark Web Research", "eye.fill"), ("Backup", "arrow.triangle.2.circlepath"),
@@ -62,37 +86,43 @@ struct SecurityAssessmentView: View {
                                 let itemWidth = (geometry.size.width / 4) - 30 // Calculate dynamic width for each item
                                 let iconSize = itemWidth * 0.25 // Scale icon size relative to item width
                                 let textSize = itemWidth * 0.1 // Scale text size relative to item width
-                                let checkboxSize = itemWidth * 0.3 // Scale checkbox size relative to item width
 
-                                Toggle(isOn: Binding(
-                                    get: { selections[option.name] ?? false },
-                                    set: { selections[option.name] = $0 }
-                                )) {
-                                    VStack {
-                                        HStack {
-                                            Image(systemName: option.icon)
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: iconSize, height: iconSize)
-                                                .foregroundColor(.blue)
+                                VStack {
+                                    HStack {
+                                        Spacer()
+                                        Image(systemName: option.icon)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: iconSize, height: iconSize)
+                                            .foregroundColor(.blue)
+                                        Spacer()
+                                    }
+                                    .padding(.top, 10)
 
-                                            Spacer()
-
-                                            Image(systemName: selections[option.name] ?? false ? "checkmark.square.fill" : "square")
-                                                .foregroundColor(selections[option.name] ?? false ? .blue : .gray)
-                                                .font(.system(size: checkboxSize)) // Scale size for checkboxes
-                                        }
-                                        .padding(.top, 10)
-
-                                        Text(option.name)
-                                            .font(.system(size: textSize)) // Scale size for text
-                                            .multilineTextAlignment(.leading)
+                                    Text(option.name)
+                                        .font(.system(size: textSize)) // Scale size for text
+                                        .multilineTextAlignment(.center)
+                                }
+                                .frame(width: itemWidth, height: itemWidth)
+                                .background(selectedStatus[option.name]?.color.opacity(0.3) ?? Color(.systemGray6))
+                                .cornerRadius(10)
+                                .onTapGesture {
+                                    if selectedCustomer == nil {
+                                        showErrorMessage = true
+                                    } else {
+                                        statusToUpdate = option.name
+                                        showStatusSelection = true
                                     }
                                 }
-                                .toggleStyle(CustomCheckboxToggleStyle())
-                                .frame(width: itemWidth, height: itemWidth)
-                                .background(Color(.systemGray6))
-                                .cornerRadius(10)
+                                .sheet(isPresented: $showStatusSelection) {
+                                    StatusSelectionView(selectedStatus: Binding(
+                                        get: { selectedStatus[statusToUpdate ?? ""] },
+                                        set: { selectedStatus[statusToUpdate ?? ""] = $0 }
+                                    ), statusName: option.name)
+                                }
+                                .alert(isPresented: $showErrorMessage) {
+                                    Alert(title: Text("Error"), message: Text("Please select a company before beginning the assessment."), dismissButton: .default(Text("OK")))
+                                }
                             }
                         }
                         .padding()
@@ -111,16 +141,52 @@ struct SecurityAssessmentView: View {
     }
 }
 
-// Custom Checkbox Toggle Style
-struct CustomCheckboxToggleStyle: ToggleStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        Button(action: { configuration.isOn.toggle() }) {
-            HStack {
-                configuration.label
-                Spacer()
+// Status selection view
+struct StatusSelectionView: View {
+    @Binding var selectedStatus: SecurityAssessmentView.Status?
+    var statusName: String
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        VStack {
+            Text("\(statusName) Status")
+                .font(.headline)
+                .padding()
+
+            ForEach(SecurityAssessmentView.Status.allCases, id: \.self) { status in
+                Button(action: {
+                    selectedStatus = status
+                }) {
+                    HStack {
+                        Text(status.description)
+                            .font(selectedStatus == status ? .headline.bold() : .body)
+                        Spacer()
+                    }
+                    .padding()
+                    .background(selectedStatus == status ? status.color : Color.clear)
+                    .foregroundColor(selectedStatus == status ? .white : .black)
+                    .cornerRadius(10)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(selectedStatus == status ? Color.clear : Color.gray))
+                }
+                .padding(.bottom, 10)
+            }
+
+            Text("Why is this important?")
+                .font(.headline)
+                .padding(.top)
+
+            Text("They are all important!")
+                .padding()
+
+            Spacer()
+
+            Button("Done") {
+                // Dismiss the modal
+                dismiss()
             }
             .padding()
         }
+        .padding()
     }
 }
 
